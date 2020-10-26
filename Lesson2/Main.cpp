896f8d3b23d851107a5cc221b972239a92176f73
Aiste Simonaityte <aiste0424@gmail.com>
2020-10-26 16:34:22 +0000

    worked with array and for loop

--- Lesson2/Main.cpp
+++ Lesson2/Main.cpp
@@ -1,18 +1,31 @@
 #include <iostream>
 #include <string>
 
+
+
+const int TOTAL = 10; 
+
 int main()
 {
 	using namespace std;
 
-	cout << "Hello world!" << endl;
+	int numbers[TOTAL] = { 78, 42, 7, 8, 15, 2, 83, 64, 23, 13 };
+
+	for (int i : numbers) {
+
+		if (i % 2 == 0) cout << i << " is even" << endl;
+		else cout << i << " is odd" << endl;
+
+	}
 
 
 
 
 
 
-	system("pause");
+
+
+
+	system("PAUSE");
 	return 0;
 }
-
